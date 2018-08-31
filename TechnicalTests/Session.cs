e0453b92326733d5cab433bdba992677e30289a0
DESKTOP-OD49FTC\Hebert <heberth26@gmail.com>
2018-08-31 14:52:17 +0100

    RaceConditionTests: Fixed bug when updating the number of session per session
    Ensuring atomicity for increment session count

--- TechnicalTests/Session.cs
+++ TechnicalTests/Session.cs
@@ -1,8 +1,16 @@
+using System.Threading;
+
 namespace TechnicalTests
 {
     public class Session : ISession
     {
-        public int Count { get; set; }
+        private int _count;
+
+        public int Count
+        {
+            get => _count;
+            set => Interlocked.Increment(ref _count);
+        }
 
         public string Key { get; set; }
     }
