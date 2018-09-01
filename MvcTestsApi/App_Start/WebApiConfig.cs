e2c586765e7bfbe178fa84b1eabaf839703cb914
DESKTOP-OD49FTC\Hebert <heberth26@gmail.com>
2018-09-01 09:12:37 +0100

    Added new route for invitationdigital  with optional parameter
    In the MvcTests project create a route to /invitationdigital/tests/{index} that takes an optional integer parameter index.

--- MvcTestsApi/App_Start/WebApiConfig.cs
+++ MvcTestsApi/App_Start/WebApiConfig.cs
@@ -7,6 +7,12 @@ namespace MvcTestsApi
         public static void Register(HttpConfiguration config)
         {
             config.Routes.MapHttpRoute(
+                name: "Invitationdigital",
+                routeTemplate: "invitationdigital/{test}/{index}",
+                defaults: new { index = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
